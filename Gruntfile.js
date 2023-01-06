745bc99ada0e2584034e093768e2f7e2887dad27
oan <oan@matterway.io>
2023-01-06 12:10:51 +0200

    add-ejs-and-templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    ejs: {
+      all: {
+        src: ['app/**/*.ejs', '!app/partials/**/*'],
+        dest: 'dist/',
+        expand: true,
+        ext: '.html',
+      },
+    },
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
@@ -18,10 +26,15 @@ module.exports = function(grunt) {
       css: {
         files: '**/*.scss',
         tasks: ['sass']
+      },
+      html: {
+        files: '**/*.ejs',
+        tasks: ['ejs']
       }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-ejs');
   grunt.registerTask('default',['watch']);
 }
